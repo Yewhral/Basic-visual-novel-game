afcf49352ea7d6cda508a74d3d326ccfa153fb3d
Yewhral <zabrzyjewski@gmail.com>
2017-06-06 21:40:29 +0200

    Stylesheet added

--- scripts/game.js
+++ scripts/game.js
@@ -1,21 +1,34 @@
+
 var game = new Phaser.Game(
     settings.width,
     settings.height,
     settings.renderer,
-    settings.parent
-);
+    'settings.parent',
+    {
+      preload: preload,
+      create: create,
+      update: update
+    });
 
 
 
 function preload() {
 
+    game.load.image('imageKey', 'images/test.jpg');
+    game.load.spritesheet('uniqueKey', 'images/test.png', 150, 150, 5);
+
+
 }
 
-var platforms;
 
 function create() {
 
 
+    game.add.sprite(0, 0, 'imageKey');
+
+    var sprite = game.add.sprite(300, 200, 'uniqueKey');
+    sprite.animations.add('walk');
+    sprite.animations.play('walk', 5, true);
 }
 function update() {
 }
