0b14ce3c7b1b418531ce90b4704f6abad4b681c6
Yewhral <zabrzyjewski@gmail.com>
2017-06-06 19:03:17 +0200

    Gulpfile prepared

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,23 @@
 var gulp = require('gulp');
 
+var folders = [
+    "./configs/*",
+    "./fonts/*",
+    "./images/*",
+    "./libs/*",
+    "./scripts/*",
+    "./styles/*",
+    "./index.html"
+];
+
+
+gulp.task('createBuild', function(){
+    return gulp.src(folders,{base:'./'})
+        .pipe(gulp.dest('./build/'))
+
+});
+
+
 gulp.task('default', function() {
 
 
