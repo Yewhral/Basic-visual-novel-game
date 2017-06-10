48de516b11ff76ec3bb7a763c9b3bd8267f39eca
Yewhral <zabrzyjewski@gmail.com>
2017-06-10 23:28:53 +0200

    Better text handling

--- configs/textContent.js
+++ configs/textContent.js
@@ -25,12 +25,14 @@ var textContent = {
         1:[
             'this is a short text for info screen on small window',
             'let\'s see how well is will work here and let\'s set up some different options',
-
             'more line because testing needs',
             'more than one'
         ],
         2:[
-
+            'this is another short text for info screen',
+            'let\'s see how well can I handle this situation and let\'s set up some different options',
+            'global counter of screen clicks maybe?',
+            'reset to zero after every scene switch'
         ]
     }
 
--- scripts/gamestates/info.js
+++ scripts/gamestates/info.js
@@ -1,37 +1,31 @@
 var gameInfo = {
 
-    create:function(){
+    create: function () {
 
         game.add.sprite(0, 0, 'gameInfoBackground');
 
         var smallBackground = game.add.sprite(0, 516, 'textBackgroundSmall');
 
         smallBackground.inputEnabled = true;
-        smallBackground.events.onInputDown.add(this.flashScreen,this);
+        smallBackground.events.onInputDown.add(this.nextLine, this);
 
-
-        game.add.button(250,0,'mainMenuStartButton',this.goBack,this,0,1,2);
-
+        game.add.button(250, 0, 'mainMenuStartButton', this.goBack, this, 0, 1, 2);
 
         text = game.add.text(settings.smallBgTextX, settings.smallBgTextY, '', {font: "24px Arial", fill: "#ffffff"});
         text.stroke = settings.textStroke;
         text.strokeThickness = settings.textStrokeThickness;
         text.fill = settings.textFill;
-        this.nextLine();
-
-        var arrows = game.add.sprite(1160,716,'proceedArrows');
-
-        arrows.animations.add('next');
-        arrows.animations.play('next',3,true);
-
-
-
+        //this.nextLine();                                                                    // Auto text start on scene creation
     },
+
     nextLine: function () {
         if (lineIndex === textContent.infoScreenText[1].length) {
+            var arrows = game.add.sprite(1160, 716, 'proceedArrows');                         // Adds arrows and animation when whole text is displayed
+            arrows.animations.add('next');
+            arrows.animations.play('next', 3, true);
             return;
         }
-        line = textContent.infoScreenText[1][lineIndex].split(' ');                             //  Splitting on spaces because word by word showing up
+        line = textContent.infoScreenText[1][lineIndex].split(' ');                           //  Splitting on spaces because word by word showing up
         wordIndex = 0;                                                                        //  Reset the word index to zero (the first word in the line)
         game.time.events.repeat(settings.wordDelay, line.length, this.nextWord, this);        //  Call the 'nextWord' function once for each word in the line (line.length)
         lineIndex++;                                                                          //  Advance to the next line
@@ -45,21 +39,13 @@ var gameInfo = {
             game.time.events.add(settings.lineDelay, this.nextLine, this);                   // Get the next line after the lineDelay amount of ms
         }
     },
-    update:function(){
-
-
-
+    clearText: function(){
+      text.text = '';
     },
 
-    goBack:function(){
+    goBack: function () {
         game.state.start('menu');
-    },
-
-
-    flashScreen:function(){
-        game.camera.flash(0xff0000, 200);
-
-
+        wordIndex = 0;
+        lineIndex = 0;
     }
-
 };
