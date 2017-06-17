41410a2cd75ef44e579f19a526cdbb2f7d617963
Yewhral <zabrzyjewski@gmail.com>
2017-06-17 18:25:33 +0200

    Small text size changes for better mobile experience

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,11 @@ let gulp = require('gulp');
 let clean = require ('gulp-clean');
 let inject = require ('gulp-inject');
 
+let uglify = require('gulp-uglify');    // TODO apply to .js files
+//let pump = require('pump');
+
+let concat = require('gulp-concat'); // TODO apply to gamestates
+
 // TODO prevent gulp from overwritting source files for safety reasons
 
 let folders = [
@@ -13,7 +18,6 @@ let folders = [
     "./styles/*",
     "./index.html"
 ];
-
 
 gulp.task('clear', function () {
     return gulp.src('./build')
@@ -28,7 +32,6 @@ gulp.task('injectGamestates', function () {
         .pipe(gulp.dest(''));
 });
 
-
 gulp.task('default', ['injectGamestates','clear'],function(){
     return gulp.src(folders,{base:'./'})
         .pipe(gulp.dest('./build/'))
@@ -36,3 +39,33 @@ gulp.task('default', ['injectGamestates','clear'],function(){
 });
 
 
+/*
+gulp.task('injectRelease',function(){
+    let target = gulp.src('release/index.html');
+    let sources = gulp.src(['release/scripts/*'], {read: false});
+
+    return target.pipe(inject(sources,{relative:true}))
+        .pipe(gulp.dest('release/index.html'));
+});
+
+gulp.task('clearRelease', function () {
+    return gulp.src('./release')
+        .pipe(clean())
+});
+
+gulp.task('moveRelease',function(){
+  return gulp.src(folders,{base:'./'})
+      .pipe(gulp.dest('release/'))
+});
+
+gulp.task('concatRelease', function(){
+    return gulp.src('./scripts/gamestates/**')
+        .pipe(concat('gamestates.min.js'))
+        .pipe(gulp.dest('release/scripts/'));
+});
+
+gulp.task('release',['clearRelease','moveRelease','concatRelease' ],function(){
+
+});
+
+*/
